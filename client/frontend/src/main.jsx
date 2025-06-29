71d3dccc38e5d749943c881876c62aebbd0f7e0d
satyajeet91201 <100056174+satyajeet91201@users.noreply.github.com>
2025-06-29 18:11:19 +0530

    Update main.jsx

--- client/frontend/src/main.jsx
+++ client/frontend/src/main.jsx
@@ -1,15 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { BrowserRouter } from 'react-router-dom'
-import { AppContextProvider } from './context/AppContext.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import { BrowserRouter } from 'react-router-dom';
+import axios from "axios";
+import { AppContextProvider } from './context/AppContext.jsx';
+
+// ✅ Global Axios Configuration
+axios.defaults.withCredentials = true;
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <AppContextProvider>
-      <App/>
-    </AppContextProvider>
-  </BrowserRouter>
-  ,
-)
+  <StrictMode>
+    <BrowserRouter>
+      <AppContextProvider>
+        <App />
+      </AppContextProvider>
+    </BrowserRouter>
+  </StrictMode>
+);
