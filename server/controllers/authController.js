51d429cbe6b4dbfec07266d22472217082944428
satyajeet91201 <100056174+satyajeet91201@users.noreply.github.com>
2025-06-30 18:45:59 +0530

    Update authController.js

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -121,8 +121,8 @@ export const logout = (req, res) => {
   try {
     res.clearCookie("token", {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+      secure: true,
+      sameSite: "none",
     });
 
     return res.status(200).json({
