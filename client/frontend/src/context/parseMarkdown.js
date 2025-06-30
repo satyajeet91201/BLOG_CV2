4593e7f3a3fa93f09a8cfa71ccce4408f75e019b
satyajeet91201 <100056174+satyajeet91201@users.noreply.github.com>
2025-06-30 13:47:16 +0530

    Update parseMarkdown.js

--- client/frontend/src/context/parseMarkdown.js
+++ client/frontend/src/context/parseMarkdown.js
@@ -1,10 +1,1 @@
-export const parseMarkdown = (text) => {
-  if (!text) return "";
 
-  let html = text
-    .replace(/\*\*(.*?)\*\*/g, '<strong>$1</strong>') // Bold
-    .replace(/\*(.*?)\*/g, '<em>$1</em>')             // Italic
-    .replace(/\n/g, '<br/>');                         // Line breaks
-
-  return html;
-};
