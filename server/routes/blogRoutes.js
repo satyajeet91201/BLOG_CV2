d92c7deb5e0b7cf08d33c6f1d6a4480016eaa896
satyajeet91201 <100056174+satyajeet91201@users.noreply.github.com>
2025-07-01 23:36:36 +0530

    Update blogRoutes.js

--- server/routes/blogRoutes.js
+++ server/routes/blogRoutes.js
@@ -1,19 +1,32 @@
 import express from "express";
-import { createBlog, getAllBlogs, likeBlog, editBlog, deleteBlog, addComment , getSingleBlog, getMyBlogs} from "../controllers/blogController.js";
+import {
+  createBlog,
+  getAllBlogs,
+  likeBlog,
+  editBlog,
+  deleteBlog,
+  addComment,
+  getSingleBlog,
+  getMyBlogs,
+} from "../controllers/blogController.js";
 import { userAuth } from "../middleware/userAuth.js";
 import { adminOnly } from "../middleware/adminAuth.js";
+import { upload } from "../middleware/uploadMiddleware.js";
 
 const router = express.Router();
 
-// Only admin can create
-router.post("/create", userAuth, adminOnly, createBlog);
+// Create Blog (Admin only + with thumbnail)
+router.post("/create", userAuth, adminOnly, upload.single('thumbnail'), createBlog);
+
+// Edit Blog (Admin only + with thumbnail)
+router.put("/edit/:id", userAuth, adminOnly, upload.single('thumbnail'), editBlog);
+
+// Other routes
 router.get("/", userAuth, getAllBlogs);
 router.get("/:id", userAuth, getSingleBlog);
 router.get("/my", userAuth, getMyBlogs);
-router.put("/edit/:id", userAuth, adminOnly, editBlog);
 router.delete("/:id", userAuth, adminOnly, deleteBlog);
 router.put("/like/:id", userAuth, likeBlog);
 router.post("/comment/:id", userAuth, addComment);
-
 
 export default router;
